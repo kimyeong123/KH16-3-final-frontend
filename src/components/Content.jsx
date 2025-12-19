9da8b4f257faad1a398ce2fdbf97384491f0f5b0
leeminsu-ai <dlalstn85451569@gmail.com>
2025-12-19 16:27:14 +0900

    상품수정

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,145 +1,154 @@
 import { Route, Routes } from "react-router-dom";
+
+// 공통 및 에러 처리
 import Home from "./Home";
+import Unauthorization from "./error/Unauthorization";
+
+// 가드(Guard) 컴포넌트
 import Private from "./guard/Private";
+import Admin from "./guard/Admin";
+import RequireSignupAgree from "../components/guard/RequireSignupAgree";
+
+// 회원(Member) 관련
 import MemberJoin from "./member/MemberJoin";
 import MemberLogin from "./member/MemberLogin";
-import MessageList from "./message/MessageList";
-import MessageDetail from "./message/MessageDetail";
+import MemberJoinFinish from "./member/MemberJoinFinish";
+import MemberMypage from "./member/MemberMypage";
+
+// 관리자(Admin) 관련
+import AdminHome from "./admin/AdminHome";
+import AdminQnaList from "./admin/AdminQnaList";
+import MemberManage from "./admin/MemberManage";
+
+// 게시판(Board) & 문의(Qna) 관련
 import BoardWrite from "./board/BoardWrite";
 import BoardList from "./board/BoardList";
 import BoardDetail from "./board/BoardDetail";
 import BoardEdit from "./board/BoardEdit";
 import QnaWrite from "./qna/QnaWrite";
 import QnaDetail from "./qna/QnaDetail";
-import MemberJoinFinish from "./member/MemberJoinFinish";
-import MemberMypage from "./member/MemberMypage";
+import QnaMain from "./qna/QnaMain";
+
+// 메시지(Message) 관련
+import MessageList from "./message/MessageList";
+import MessageDetail from "./message/MessageDetail";
+import MessageWrite from "./message/MessageWrite";
+
+// 결제(Pay) 관련
 import KakaoPay from "./pay/KakaoPay";
 import KakaoPaySuccess from "./pay/KakaoPaySuccess";
 import KakaoPayCancel from "./pay/KakaoPayCancel";
 import KakaoPayFail from "./pay/KakaoPayFail";
+import Exchange from "./pay/Exchange";
+
+// 상품(Product) 관련
 import ProductAdd from "./product/ProductAdd";
 import ProductAddDone from "./product/ProductAddDone";
-import Admin from "./guard/Admin";
-import MessageWrite from "./message/MessageWrite";
-import AdminHome from "./admin/AdminHome";
-import AdminQnaList from "./admin/AdminQnaList";
-import MemberManage from "./admin/MemberManage";
-import Unauthorization from "./error/Unauthorization";
 import ProductDetail from "./product/ProductDetail";
 import ProductEdit from "./product/ProductEdit";
 import ProductList from "./product/ProductList";
-import Exchange from "./pay/Exchange";
 import ProductMyList from "./product/ProductMyList";
+// ▼▼▼ [새로 추가된 구매 내역 컴포넌트] ▼▼▼
+import ProductPurchaseList from "./product/ProductPurchaseList"; 
 import ProductAuctionList from "./product/ProductAuctionList";
 import AuctionDetail from "./product/AuctionDetail";
-import QnaMain from "./qna/QnaMain";
+
+// 기타(Etc) 약관 관련
 import Terms from "./etc/Terms";
 import SignupAgreement from "./etc/SignupAgreement";
 import PrivacyPolicy from "./etc/PrivacyPolicy";
-import RequireSignupAgree from "../components/guard/RequireSignupAgree";
-
-
-
 
 export default function Content() {
-
-    return (<>
+    return (
         <div className="row">
             <div className="col-md-10 offset-md-1">
                 <Routes>
-                    {/* 메인페이지 */}
-                    <Route path="/" element={<Home />}></Route>
-                    {/* 에러페이지 */}
+                    {/* ================= 메인 & 에러 ================= */}
+                    <Route path="/" element={<Home />} />
                     <Route path="/error/403" element={<Unauthorization />} />
 
-                    {/* 회원 관련 페이지들 */}
-
+                    {/* ================= 회원 관련 ================= */}
                     <Route path="/signup-agreement" element={<SignupAgreement />} />
-                    <Route
-                        path="/member/join"
+                    <Route 
+                        path="/member/join" 
                         element={
                             <RequireSignupAgree>
                                 <MemberJoin />
                             </RequireSignupAgree>
-                        }
+                        } 
                     />
-                    <Route path="/member/login" element={<MemberLogin />}></Route>
-                    <Route path="/member/joinfinish" element={<MemberJoinFinish />}></Route>
-                    <Route path="/member/mypage" element={<Private><MemberMypage /></Private>}></Route>
+                    <Route path="/member/login" element={<MemberLogin />} />
+                    <Route path="/member/joinfinish" element={<MemberJoinFinish />} />
+                    
+                    {/* 마이페이지 */}
+                    <Route path="/member/mypage" element={<Private><MemberMypage /></Private>} />
                     <Route path="/member/mypage/:memberNo" element={<MemberMypage />} />
 
-                    {/* 관리자 홈 */}
+                    {/* ================= 관리자 관련 ================= */}
                     <Route path="/admin/home" element={<Admin><AdminHome /></Admin>}>
-                        <Route
-                            index
-                            element={<div>관리자 대시보드</div>}
-                        />
+                        <Route index element={<div>관리자 대시보드</div>} />
                         <Route path="member" element={<MemberManage />} />
                         <Route path="qnalist" element={<AdminQnaList />} />
                     </Route>
 
-                    {/* 게시글 페이지(공지만) - HEAD 부분 */}
-                    <Route path="/board/write" element={<Admin><BoardWrite /></Admin>}></Route>
-                    <Route path="/board/list" element={<BoardList />}></Route>
-                    <Route path="/board/:boardNo" element={<BoardDetail />}></Route>
-                    <Route path="/board/edit/:boardNo" element={<Admin><BoardEdit /></Admin>}></Route>
+                    {/* ================= 게시판 & 문의 ================= */}
+                    <Route path="/board/list" element={<BoardList />} />
+                    <Route path="/board/write" element={<Admin><BoardWrite /></Admin>} />
+                    <Route path="/board/:boardNo" element={<BoardDetail />} />
+                    {/* (구버전 경로 호환용) */}
+                    <Route path="/board/detail/:boardNo" element={<BoardDetail />} />
+                    <Route path="/board/edit/:boardNo" element={<Admin><BoardEdit /></Admin>} />
+                    <Route path="/board/edit" element={<BoardEdit />} />
 
-                    {/* 문의 페이지(board 재활용) - HEAD 부분 */}
-                    <Route path="/qna/write" element={<Private><QnaWrite /></Private>}></Route>
-                    <Route path="/qna/:boardNo" element={<QnaDetail />}></Route>
-                    <Route path="/qna/main" element={<QnaMain />}></Route>
-                    {/* 관리자용 QNA List는 따로 만들면 좋을듯 */}
+                    <Route path="/qna/main" element={<QnaMain />} />
+                    <Route path="/qna/write" element={<Private><QnaWrite /></Private>} />
+                    <Route path="/qna/:boardNo" element={<QnaDetail />} />
 
+                    {/* ================= 메시지(알림) ================= */}
+                    <Route path="/message/list" element={<Private><MessageList /></Private>} />
+                    <Route path="/message/write" element={<Private><MessageWrite /></Private>} />
+                    <Route path="/message/:messageNo" element={<Private><MessageDetail /></Private>} />
 
-                    {/* 메세지(알림) - HEAD 부분 */}
-                    <Route path="/message/list" element={<Private><MessageList /></Private>}></Route>
-                    <Route path="/message/:messageNo" element={<Private><MessageDetail /></Private>}></Route>
-                    <Route path="/message/write" element={<Private><MessageWrite /></Private>}></Route>
-
-                    {/* HEAD 브랜치 결제 라우트 */}
+                    {/* ================= 결제 관련 ================= */}
                     <Route path="/pay/kakaopay" element={<KakaoPay />} />
+                    <Route path="/pay/exchange" element={<Exchange />} />
+                    
+                    {/* 카카오페이 리다이렉트 (슬래시 스타일 / CamelCase 스타일 둘 다 지원) */}
                     <Route path="/pay/kakaopay/success" element={<KakaoPaySuccess />} />
                     <Route path="/pay/kakaopay/cancel" element={<KakaoPayCancel />} />
                     <Route path="/pay/kakaopay/fail" element={<KakaoPayFail />} />
-
-                    {/*  상품 등록 페이지 - HEAD 부분 */}
-                    <Route path="/product/productadd" element={<ProductAdd />} />
-                    <Route path="/product/done" element={<ProductAddDone />} />
-
-
-                    {/* 게시글 페이지 - origin/main 부분 (경로 중복 발생 가능) */}
-                    <Route path="board/write" element={<Private><BoardWrite /></Private>}></Route>
-                    <Route path="board/list" element={<BoardList />}></Route>
-                    <Route path="board/detail/:boardNo" element={<BoardDetail />}></Route>
-                    <Route path="board/edit" element={<BoardEdit />}></Route>
-
-                    {/* 카카오페이 - origin/main 부분 (경로 중복 발생 가능) */}
-                    <Route path="/pay/kakaopay" element={<KakaoPay />} />
+                    
                     <Route path="/pay/kakaopaySuccess" element={<KakaoPaySuccess />} />
                     <Route path="/pay/kakaopayCancel" element={<KakaoPayCancel />} />
                     <Route path="/pay/kakaopayFail" element={<KakaoPayFail />} />
-                    <Route path="/pay/exchange" element={<Exchange />} />
 
-                    {/*  상품 등록/목록 페이지 - origin/main 부분 */}
+                    {/* ================= 상품 관련 ================= */}
+                    <Route path="/product/list" element={<ProductList />} />
                     <Route path="/product/productadd" element={<ProductAdd />} />
                     <Route path="/product/done" element={<ProductAddDone />} />
-                    <Route path="/product/list" element={<ProductList />} />
                     <Route path="/product/detail/:productNo" element={<ProductDetail />} />
                     <Route path="/product/edit/:productNo" element={<ProductEdit />} />
-                    <Route path="/product/mylist" element={<ProductMyList />} />
+                    
+                    {/* 내 판매 목록 */}
+                    <Route path="/product/mylist" element={<Private><ProductMyList /></Private>} />
+
+                    {/* ▼▼▼ [여기!] 내 구매/입찰 목록 (새로 만든 페이지) ▼▼▼ */}
+                    <Route path="/product/purchase" element={<Private><ProductPurchaseList /></Private>} />
+
+                    {/* 경매 리스트 및 상세 */}
                     <Route path="/product/auction/list" element={<ProductAuctionList />} />
-                    <Route path="/product/auction/detail/:productNo" element={<AuctionDetail />} />
-                    <Route path="/product/auction/list/" element={<ProductAuctionList />} />
                     <Route path="/product/auction/list/*" element={<ProductAuctionList />} />
+                    <Route path="/product/auction/detail/:productNo" element={<AuctionDetail />} />
 
-                    {/* 기타 */}
-                    <Route path="etc/terms" element={<Terms />} />
-                    <Route path="etc/privacy" element={<PrivacyPolicy />} />
-                    <Route path="etc/signupagree" element={<SignupAgreement />} />
+                    {/* ================= 기타 약관 ================= */}
+                    <Route path="/etc/terms" element={<Terms />} />
+                    <Route path="/etc/privacy" element={<PrivacyPolicy />} />
+                    <Route path="/etc/signupagree" element={<SignupAgreement />} />
 
-
+                    {/* ================= 404 페이지 ================= */}
+                    <Route path="*" element={<div className="p-5 text-center"><h3>페이지를 찾을 수 없습니다 (404)</h3></div>} />
                 </Routes>
             </div>
         </div>
-    </>)
+    );
 }
--- src/components/product/ProductMyList.jsx
+++ src/components/product/ProductMyList.jsx
@@ -4,20 +4,25 @@ import { useAtom } from "jotai";
 import { accessTokenState } from "../../utils/jotai";
 import { useNavigate } from "react-router-dom";
 
+// ▼▼▼ [추가] 구매 내역 컴포넌트 임포트 (같은 폴더에 있다고 가정)
+import ProductPurchaseList from "./ProductPurchaseList"; 
+
 export default function ProductMyList() {
   const navigate = useNavigate();
   
-  // Jotai의 atomWithStorage와 연동되어 새로고침 시에도 토큰이 유지됩니다.
+  // === [추가] 탭 상태 관리 ('SALES': 판매내역, 'PURCHASE': 구매내역) ===
+  const [activeTab, setActiveTab] = useState("SALES");
+
+  // Jotai
   const [accessToken, setAccessToken] = useAtom(accessTokenState);
 
+  // === 판매 내역 관련 State ===
   const [page, setPage] = useState(1);
   const [vo, setVo] = useState({ list: [], last: true });
   const [loading, setLoading] = useState(false);
-  
-  // 썸네일 번호가 없는 상품을 위해 추가로 번호를 저장하는 상태
   const [thumbNoByProduct, setThumbNoByProduct] = useState({}); 
 
-  // 인증 헤더 설정
+  // 인증 헤더
   const authHeader = useMemo(() => {
     if (!accessToken) return "";
     return accessToken.startsWith("Bearer ") ? accessToken : "Bearer " + accessToken;
@@ -28,7 +33,6 @@ export default function ProductMyList() {
   const money = (v) => (v ? Number(v).toLocaleString() : "-");
   const dt = (v) => (v ? new Date(v).toLocaleString() : "-");
   
-  // 데이터 객체에서 안전하게 값을 꺼내오는 함수 (CamelCase/SnakeCase 호환)
   const get = (obj, keys) => {
     if (!obj) return undefined;
     for (const k of keys) {
@@ -38,9 +42,9 @@ export default function ProductMyList() {
     return undefined;
   };
 
-  // === 데이터 불러오기 ===
+  // === 데이터 불러오기 (판매 내역용) ===
   const fetchList = async (targetPage) => {
-    if (!accessToken) return;
+    if (!accessToken || activeTab !== "SALES") return; // 탭이 다르면 요청 안 함
 
     setLoading(true);
     try {
@@ -48,7 +52,6 @@ export default function ProductMyList() {
         headers: { Authorization: authHeader },
       });
 
-      // 토큰 갱신 로직
       const renewed = resp.headers["access-token"] || resp.headers["Access-Token"];
       if (renewed) setAccessToken(renewed);
 
@@ -67,13 +70,15 @@ export default function ProductMyList() {
     }
   };
 
-  // 1. 페이지나 토큰 변경 시 데이터 로드
+  // useEffect: 탭이 'SALES'일 때만 데이터 로드
   useEffect(() => {
-    fetchList(page);
+    if (activeTab === "SALES") {
+        fetchList(page);
+    }
     // eslint-disable-next-line
-  }, [page, accessToken]); 
+  }, [page, accessToken, activeTab]); 
 
-  // 2. 썸네일 번호 추출 (객체 내부에서 번호 찾기)
+  // 썸네일 로직
   const resolveThumbNo = (p) => {
     const atts = get(p, ["attachments", "attachmentList"]);
     if (Array.isArray(atts) && atts.length > 0) {
@@ -82,9 +87,8 @@ export default function ProductMyList() {
     return get(p, ["thumbnailAttachmentNo", "thumbnail_attachment_no"]);
   };
 
-  // 3. 썸네일 번호가 없는 경우에만 추가 서버 통신
   useEffect(() => {
-    if (!vo.list.length) return;
+    if (activeTab !== "SALES" || !vo.list.length) return;
     let alive = true;
 
     const run = async () => {
@@ -115,9 +119,9 @@ export default function ProductMyList() {
     };
     run();
     return () => { alive = false; };
-  }, [vo.list]);
+  }, [vo.list, activeTab]);
 
-  // === 버튼 기능 ===
+  // 버튼 기능
   const goDetail = (pNo, status) => {
     if (status === 'BIDDING') navigate(`/product/auction/detail/${pNo}`);
     else navigate(`/product/detail/${pNo}`);
@@ -136,108 +140,149 @@ export default function ProductMyList() {
     }
   };
 
+  // === 스타일 ===
+  const tabStyle = (isActive) => ({
+      padding: "12px 24px",
+      fontSize: "16px",
+      fontWeight: "bold",
+      cursor: "pointer",
+      borderBottom: isActive ? "3px solid #333" : "3px solid transparent",
+      color: isActive ? "#333" : "#999",
+      background: "transparent",
+      border: "none",
+      borderBottom: isActive ? "3px solid #333" : "1px solid #ddd",
+      flex: 1,
+      transition: "0.2s"
+  });
+
   return (
-    <div style={{ maxWidth: 1200, margin: "0 auto", padding: 24 }}>
-      <h2 style={{ marginBottom: 20, fontWeight: 900, textAlign: "center" }}>내 물품등록 목록</h2>
-
-      <div style={{ marginBottom: 15 }}>
-        <button 
-          onClick={() => navigate("/product/productadd")} 
-          style={{ padding: "10px 16px", background: "#333", color: "white", border: "none", borderRadius: 4, cursor: "pointer", fontWeight: "bold" }}
-        >
-          물품등록
-        </button>
+    <div style={{ maxWidth: 1200, margin: "0 auto", padding: "40px 24px" }}>
+      
+      {/* === [탭 버튼 영역] === */}
+      <div style={{ display: "flex", marginBottom: "30px", borderBottom: "1px solid #ddd" }}>
+          <button 
+            style={tabStyle(activeTab === "SALES")}
+            onClick={() => setActiveTab("SALES")}
+          >
+            내 판매 내역 (물품등록)
+          </button>
+          <button 
+            style={tabStyle(activeTab === "PURCHASE")}
+            onClick={() => setActiveTab("PURCHASE")}
+          >
+            내 구매/입찰 내역
+          </button>
       </div>
 
-      <div style={{ border: "1px solid #ddd", borderRadius: 8, overflow: "hidden", background: "white" }}>
-        <table style={{ width: "100%", borderCollapse: "collapse" }}>
-          <thead>
-            <tr style={{ background: "#f8f9fa", borderBottom: "2px solid #eee" }}>
-              <th style={{ padding: 12, textAlign: "left" }}>이미지</th>
-              <th style={{ padding: 12, textAlign: "left" }}>번호</th>
-              <th style={{ padding: 12, textAlign: "left" }}>상품명</th>
-              <th style={{ padding: 12, textAlign: "right" }}>시작가</th>
-              <th style={{ padding: 12, textAlign: "right" }}>현재가</th>
-              <th style={{ padding: 12, textAlign: "center" }}>상태</th>
-              <th style={{ padding: 12, textAlign: "center" }}>시작일</th>
-              <th style={{ padding: 12, textAlign: "center" }}>마감일</th>
-              <th style={{ padding: 12, textAlign: "center" }}>관리</th>
-            </tr>
-          </thead>
-          <tbody>
-            {vo.list.length === 0 && !loading && (
-              <tr>
-                <td colSpan={9} style={{ padding: 50, textAlign: "center", color: "#999" }}>등록된 물품이 없습니다.</td>
-              </tr>
-            )}
-            {vo.list.map((p) => {
-              const pNo = get(p, ["productNo", "product_no"]);
-              const attNo = resolveThumbNo(p) || thumbNoByProduct[pNo];
-              const status = get(p, ["status"]);
-              const isLocked = status === 'BIDDING' || status === 'ENDED' || status === 'CLOSED';
+      {/* === [조건부 렌더링 1] 구매 내역 탭 선택 시 === */}
+      {activeTab === "PURCHASE" && (
+          <ProductPurchaseList />
+      )}
 
-              return (
-                <tr key={pNo} style={{ borderBottom: "1px solid #f0f0f0" }}>
-                  <td style={{ padding: 12 }}>
-                    {attNo ? (
-                      <img src={ATT_VIEW(attNo)} alt="thumb" style={{ width: 80, height: 60, objectFit: "cover", borderRadius: 4, border: "1px solid #eee" }} />
-                    ) : (
-                      <div style={{ width: 80, height: 60, background: "#f0f0f0", borderRadius: 4 }} />
+      {/* === [조건부 렌더링 2] 판매 내역 탭 선택 시 (기존 코드) === */}
+      {activeTab === "SALES" && (
+          <>
+            <div style={{ display:"flex", justifyContent:"space-between", alignItems:"center", marginBottom: 15 }}>
+                <h3 style={{ margin: 0, fontWeight: "bold" }}>내가 등록한 물품</h3>
+                <button 
+                    onClick={() => navigate("/product/productadd")} 
+                    style={{ padding: "8px 16px", background: "#333", color: "white", border: "none", borderRadius: 4, cursor: "pointer", fontWeight: "bold" }}
+                >
+                    + 새 물품 등록
+                </button>
+            </div>
+
+            <div style={{ border: "1px solid #ddd", borderRadius: 8, overflow: "hidden", background: "white" }}>
+                <table style={{ width: "100%", borderCollapse: "collapse" }}>
+                <thead>
+                    <tr style={{ background: "#f8f9fa", borderBottom: "2px solid #eee" }}>
+                    <th style={{ padding: 12, textAlign: "left" }}>이미지</th>
+                    <th style={{ padding: 12, textAlign: "left" }}>번호</th>
+                    <th style={{ padding: 12, textAlign: "left" }}>상품명</th>
+                    <th style={{ padding: 12, textAlign: "right" }}>시작가</th>
+                    <th style={{ padding: 12, textAlign: "right" }}>현재가</th>
+                    <th style={{ padding: 12, textAlign: "center" }}>상태</th>
+                    <th style={{ padding: 12, textAlign: "center" }}>시작일</th>
+                    <th style={{ padding: 12, textAlign: "center" }}>마감일</th>
+                    <th style={{ padding: 12, textAlign: "center" }}>관리</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {vo.list.length === 0 && !loading && (
+                    <tr>
+                        <td colSpan={9} style={{ padding: 50, textAlign: "center", color: "#999" }}>등록된 물품이 없습니다.</td>
+                    </tr>
                     )}
-                  </td>
-                  <td style={{ padding: 12 }}>{pNo}</td>
-                  <td style={{ padding: 12 }}>
-                    <span 
-                      onClick={() => goDetail(pNo, status)} 
-                      style={{ cursor: "pointer", fontWeight: "bold", textDecoration: "underline" }}
-                    >
-                      {get(p, ["name", "productName"])}
-                    </span>
-                  </td>
-                  <td style={{ padding: 12, textAlign: "right" }}>{money(get(p, ["startPrice", "start_price"]))}</td>
-                  <td style={{ padding: 12, textAlign: "right", color: "#d32f2f", fontWeight: "bold" }}>
-                    {money(get(p, ["finalPrice", "final_price"])) || "-"}
-                  </td>
-                  <td style={{ padding: 12, textAlign: "center" }}>
-                    <span style={{ fontSize: 12, padding: "4px 8px", background: "#f0f0f0", borderRadius: 4, color: "#666" }}>
-                        {status}
-                    </span>
-                  </td>
-                  <td style={{ padding: 12, textAlign: "center", fontSize: 13, color: "#555" }}>
-                    {dt(get(p, ["startTime", "start_time"]))}
-                  </td>
-                  <td style={{ padding: 12, textAlign: "center", fontSize: 13, color: "#555" }}>
-                    {dt(get(p, ["endTime", "end_time"]))}
-                  </td>
-                  <td style={{ padding: 12, textAlign: "center" }}>
-                    <button 
-                      onClick={() => !isLocked && navigate(`/product/edit/${pNo}`)} 
-                      disabled={isLocked}
-                      style={{ marginRight: 5, padding: "5px 10px", cursor: isLocked ? "not-allowed" : "pointer", opacity: isLocked ? 0.3 : 1 }}
-                    >
-                      수정
-                    </button>
-                    <button 
-                      onClick={() => !isLocked && remove(pNo)} 
-                      disabled={isLocked}
-                      style={{ color: "red", padding: "5px 10px", cursor: isLocked ? "not-allowed" : "pointer", opacity: isLocked ? 0.3 : 1 }}
-                    >
-                      삭제
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+                    {vo.list.map((p) => {
+                    const pNo = get(p, ["productNo", "product_no"]);
+                    const attNo = resolveThumbNo(p) || thumbNoByProduct[pNo];
+                    const status = get(p, ["status"]);
+                    const isLocked = status === 'BIDDING' || status === 'ENDED' || status === 'CLOSED';
 
-      {/* 페이지네이션 */}
-      <div style={{ display: "flex", justifyContent: "center", gap: 10, marginTop: 20 }}>
-        <button onClick={() => setPage(p => Math.max(1, p - 1))} disabled={page <= 1} style={{ padding: "6px 12px" }}>이전</button>
-        <span style={{ padding: "6px 12px", fontWeight: "bold" }}>page {page}</span>
-        <button onClick={() => setPage(p => (vo.last ? p : p + 1))} disabled={vo.last} style={{ padding: "6px 12px" }}>다음</button>
-      </div>
+                    return (
+                        <tr key={pNo} style={{ borderBottom: "1px solid #f0f0f0" }}>
+                        <td style={{ padding: 12 }}>
+                            {attNo ? (
+                            <img src={ATT_VIEW(attNo)} alt="thumb" style={{ width: 80, height: 60, objectFit: "cover", borderRadius: 4, border: "1px solid #eee" }} />
+                            ) : (
+                            <div style={{ width: 80, height: 60, background: "#f0f0f0", borderRadius: 4 }} />
+                            )}
+                        </td>
+                        <td style={{ padding: 12 }}>{pNo}</td>
+                        <td style={{ padding: 12 }}>
+                            <span 
+                            onClick={() => goDetail(pNo, status)} 
+                            style={{ cursor: "pointer", fontWeight: "bold", textDecoration: "underline" }}
+                            >
+                            {get(p, ["name", "productName"])}
+                            </span>
+                        </td>
+                        <td style={{ padding: 12, textAlign: "right" }}>{money(get(p, ["startPrice", "start_price"]))}</td>
+                        <td style={{ padding: 12, textAlign: "right", color: "#d32f2f", fontWeight: "bold" }}>
+                            {money(get(p, ["finalPrice", "final_price"])) || "-"}
+                        </td>
+                        <td style={{ padding: 12, textAlign: "center" }}>
+                            <span style={{ fontSize: 12, padding: "4px 8px", background: "#f0f0f0", borderRadius: 4, color: "#666" }}>
+                                {status}
+                            </span>
+                        </td>
+                        <td style={{ padding: 12, textAlign: "center", fontSize: 13, color: "#555" }}>
+                            {dt(get(p, ["startTime", "start_time"]))}
+                        </td>
+                        <td style={{ padding: 12, textAlign: "center", fontSize: 13, color: "#555" }}>
+                            {dt(get(p, ["endTime", "end_time"]))}
+                        </td>
+                        <td style={{ padding: 12, textAlign: "center" }}>
+                            <button 
+                            onClick={() => !isLocked && navigate(`/product/edit/${pNo}`)} 
+                            disabled={isLocked}
+                            style={{ marginRight: 5, padding: "5px 10px", cursor: isLocked ? "not-allowed" : "pointer", opacity: isLocked ? 0.3 : 1 }}
+                            >
+                            수정
+                            </button>
+                            <button 
+                            onClick={() => !isLocked && remove(pNo)} 
+                            disabled={isLocked}
+                            style={{ color: "red", padding: "5px 10px", cursor: isLocked ? "not-allowed" : "pointer", opacity: isLocked ? 0.3 : 1 }}
+                            >
+                            삭제
+                            </button>
+                        </td>
+                        </tr>
+                    );
+                    })}
+                </tbody>
+                </table>
+            </div>
+
+            {/* 페이지네이션 (판매내역용) */}
+            <div style={{ display: "flex", justifyContent: "center", gap: 10, marginTop: 20 }}>
+                <button onClick={() => setPage(p => Math.max(1, p - 1))} disabled={page <= 1} style={{ padding: "6px 12px" }}>이전</button>
+                <span style={{ padding: "6px 12px", fontWeight: "bold" }}>page {page}</span>
+                <button onClick={() => setPage(p => (vo.last ? p : p + 1))} disabled={vo.last} style={{ padding: "6px 12px" }}>다음</button>
+            </div>
+          </>
+      )}
     </div>
   );
 }
